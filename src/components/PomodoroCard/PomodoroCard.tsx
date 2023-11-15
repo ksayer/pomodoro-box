acb9f946b84a24ba3ee68aa93f2b5575c96d8294
Nikolai Zinchenko <nikolaizinchenko94@gmail.com>
2023-11-15 20:37:52 +0400

    feat(statistic): add data from store in pomodoroCard

--- src/components/PomodoroCard/PomodoroCard.tsx
+++ src/components/PomodoroCard/PomodoroCard.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import styles from './PomodoroCard.module.css';
 import {Icon} from "../Icon";
+import {useSelector} from "react-redux";
+import {getGlobalCounter} from "../../store/slices/counter";
+
+function getPomodoroString(count:number) {
+  let ending;
+
+  if (count % 10 === 1 && count % 100 !== 11) {
+    ending = '';
+  } else if ([2, 3, 4].includes(count % 10) && ![12, 13, 14].includes(count % 100)) {
+    ending = 'а';
+  } else {
+    ending = 'ов';
+  }
+
+  return `помидор${ending}`;
+}
 
 export function PomodoroCard() {
+  const { finishedTasks } = useSelector(getGlobalCounter)
   return (
     <div className={styles.card}>
       <div className={styles.card__body}>
         <Icon name={"tomato"}/>
-        <span className={styles.card__count}>x 2</span>
+        <span className={styles.card__count}>x {finishedTasks}</span>
       </div>
-      <p className={styles.card__footer}>2 помидора</p>
+      <p className={styles.card__footer}>{finishedTasks} {getPomodoroString(finishedTasks)}</p>
     </div>
   );
 }
