5365c51a6f62131ae2baba762ab49c1acd07c696
Nikolai Zinchenko <nikolaizinchenko94@gmail.com>
2023-11-23 21:31:34 +0400

    feat(modal): animation to fadeIn for modal delete

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,11 +12,19 @@ interface IModal {
 export const Modal: FC<IModal> = ({children, isModalOpen, setIsModalOpen}) => {
   const ref = useRef(null);
   useClickOutside({ref, setIsOpen: setIsModalOpen, isOpen: isModalOpen});
+
+
   const modalContainer = document.querySelector('#modal-container');
   if (!modalContainer) return null;
   return (
     ReactDOM.createPortal(
-      <div ref={ref} className={styles.modal}>{children}</div>, modalContainer
+      <div
+        style={{
+          animation: `${styles.fadeIn} 0.3s`,
+        }}
+        ref={ref} className={`${styles.modal}`}>{children}
+      </div>
+    , modalContainer
     )
   );
 }
